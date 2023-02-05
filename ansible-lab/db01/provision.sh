814e4b6bc9cf1dc15e2f5aec19ea73fa3934d4c8
Arimateia Junior <arimateiajunior.tic@gmail.com>
2023-02-05 00:00:30 -0300

    Configurando o control-node e manager hots

---
+++ ansible-lab/db01/provision.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cat << EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCx113ZPmarASqzZO5/0IsGddxTlkfaT0pc6b1midFnO9ARa3QBNjLOmrKnoYWU9ik0ZmgtEEhObCfKB96nQtllgJwM7gZAr15Gk9hmrjvN4tY3lSJfRp8Ru8mULCbF6nbjg45Ajlaccszyirk0nNO9FXkvOzsXackGMWkk1H8HHzLyUEUQu0gTltD0PXhuvhXPwtC06i9sedPEn+qtkxpz7Fw/Ud0+U1vcN+OMA57cCHtE+3w4kd1SS/UUJ84fCEnVTS4MADyZcsIocfLwRhMmHwLahw0v/a/j7z8hny/hl0pXPJXmFwAC1Tb/L+psY9g8yRCF0a7pT4R+Y10E1BYx vagrant@control-node
+EOT
