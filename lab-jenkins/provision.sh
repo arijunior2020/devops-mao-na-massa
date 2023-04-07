2c8edc77dae466cbb9e561e9ee16512201a3f107
Arimateia Junior <arimateiajunior.tic@gmail.com>
2023-04-07 18:40:57 -0300

    Adicionando laboratório de Jenkins

---
+++ lab-jenkins/provision.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+yum install epel-release -y
+yum install wget git -y
+
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install java-11-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+##instalação do docker e docker compose
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+systemctl daemon-reload
+systemctl restart docker
+usermod -aG docker jenkins
+sudo chmod 666 /var/run/docker.sock
